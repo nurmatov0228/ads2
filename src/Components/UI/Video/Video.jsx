ff561a9508bdb34943debc916690df0200511962
nurmatov0228 <bahodirnur06@gmail.com>
2024-10-09 18:25:31 -0800

    in

---
+++ src/Components/UI/Video/Video.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./video.scss";
+
+const Video = () => {
+  return <div>video</div>;
+};
+
+export default Video;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "../styles/home.scss";
+import In from "../Components/UI/In/In";
 
 const Home = () => {
-  return <div className="home">home</div>;
+  return (
+    <div className="home">
+      home
+      <In />
+    </div>
+  );
 };
 
 export default Home;
